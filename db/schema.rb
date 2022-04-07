9befccfdabe78a4914a6bc266cf8fe1c0cafb1ce
Brett Samson <brettsamson@gmail.com>
2022-04-07 17:52:46 -0500

    Update gems

--- db/schema.rb
+++ db/schema.rb
@@ -15,7 +15,6 @@ ActiveRecord::Schema.define(version: 20160516212056) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
-  enable_extension "uuid-ossp"
 
   create_table "channels", force: :cascade do |t|
     t.integer  "user_id"
